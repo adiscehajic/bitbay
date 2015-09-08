99cea85227c4ff41bcc78b039921150666d4ca15
Adnan Lapendic <adnan.lapendic@bitcamp.ba>
2015-09-08 23:46:45 +0200

    Create Category model and action methods

--- bitbay/app/controllers/Categories.java
+++ bitbay/app/controllers/Categories.java
@@ -1,6 +1,5 @@
 package controllers;
 
-<<<<<<< HEAD
 import play.Logger;
 import play.data.Form;
 import play.mvc.Controller;
@@ -13,19 +12,13 @@ import java.lang.*;
 import com.avaje.ebean.Ebean;
 import models.*;
 
-=======
-import play.mvc.Result;
-import play.mvc.Controller;
->>>>>>> 89e26e0d4eb85f455eac3c0f54dc325cea0857db
 
 /**
  * Created by Adnan on 8.9.2015.
  */
-<<<<<<< HEAD
+
+
 public class Categories extends Controller{
-=======
-public class Categories extends Controller {
->>>>>>> 89e26e0d4eb85f455eac3c0f54dc325cea0857db
 
     /**
      * Method for creating new Category
@@ -70,13 +63,7 @@ public class Categories extends Controller {
      */
     public Result list(){
 
-<<<<<<< HEAD
         return TODO;
     }
 }
-=======
 
-        return TODO;
-    }
-}
->>>>>>> 89e26e0d4eb85f455eac3c0f54dc325cea0857db
