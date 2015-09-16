e9560c9ebacd6a8aabbd5c0ad4554c68f8f3eb79
Adnan Lapendic <adnan.lapendic@bitcamp.ba>
2015-09-16 06:37:13 +0200

    Fix bugs and finish country model and methods

--- bitbay/app/models/Country.java
+++ bitbay/app/models/Country.java
@@ -6,6 +6,7 @@ import play.data.validation.Constraints;
 
 import javax.persistence.*;
 import java.lang.String;
+import java.util.List;
 
 /**
  * Created by dinko.hodzic on 08/09/15.
@@ -19,4 +20,20 @@ public class Country extends Model{
     @Constraints.MaxLength(255)
     public String name;
     //Some comment
+
+    private  static Finder<String, Country> finder = new Finder<String, Country>(Country.class);
+
+    public static List<Country> findAllCountries() {
+        List<Country> countries = finder.findList();
+        return countries;
+    }
+
+    public static Country findCountryByName(String name){
+        for(Country c: findAllCountries()){
+            if(c.name.equals(name)){
+                return c;
+            }
+        }
+        return null;
+    }
 }
